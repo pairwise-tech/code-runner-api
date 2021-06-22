4d8fe56dcf8eae348debe91a8cf75ea2e9cc286b
Sean Smith <sean.smith.2009@gmail.com>
2021-06-23 06:06:03 +0800

    Change temp directory path

--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,7 @@ import rustCodeRunner from "./rust-code-runner";
 import pythonCodeRunner from "./python-code-runner";
 
 // Create temp directory which will store temporary code output files
-const TEMP_DIRECTORY = "./temp";
+const TEMP_DIRECTORY = "temp";
 if (!fs.existsSync(TEMP_DIRECTORY)) {
   fs.mkdirSync(TEMP_DIRECTORY);
 }
